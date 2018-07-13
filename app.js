54466c11e4b278f1ce4926e4161ef7fe3b0e467b
Killdore <killdore@outlook.com>
2018-07-13 19:44:19 +0200

    Done.

---
+++ app.js
@@ -0,0 +1,92 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const client = new Discord.Client({ disableEveryone: true })
+
+client.on('ready', () => {
+    console.log("Ready!")
+
+    client.user.setActivity("counting channels (" + fs.readFileSync('./counts.txt') + " global counts)", { type: "WATCHING" })
+    
+    setInterval(() => {
+        client.user.setActivity("counting channels (" + fs.readFileSync('./counts.txt') + " global counts)", { type: "WATCHING" })
+    }, 60000)
+})
+
+client.on('message', message => {
+    let content = message.content.toLowerCase();
+
+    if (message.author.id == client.user.id) return;
+
+    if (message.channel.id == getCountingChannel(message.guild.id)) {
+        let count = getCount(message.guild.id)[0];
+        let user = getCount(message.guild.id)[1];
+        console.log(message.author.id)
+        console.log(user)
+        console.log(message.author.id == user)
+        if (message.author.id == user) return message.delete() // we want someone else to count before the same person counts
+        if (message.content != (count + 1).toString()) return message.delete()
+        addToCount(message.guild.id, message.author.id); count += 1;
+        message.channel.setTopic("**Next count: **" + (count + 1));
+    }
+
+    if (content.startsWith("c!channel")) {
+        if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send(":x: You don't have permission!")
+        saveCountingChannel(message.guild.id, message.channel.id)
+        return message.channel.send(":white_check_mark: From now on, this channel will be used for counting.");
+    } else if (content.startsWith("c!reset")) {
+        if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send(":x: You don't have permission!")
+        resetCount(message.guild.id);
+
+        let channel = message.guild.channels.get(getCountingChannel(message.guild.id));
+        if (channel) channel.setTopic("**Next count: **1");
+        return message.channel.send(":white_check_mark: Counting has been reset.");
+    }
+
+})
+
+function saveCountingChannel(guildid, channelid) {
+    let file = JSON.parse(fs.readFileSync('./guilds.json'))
+    if (!file[guildid]) file[guildid] = {}
+    file[guildid].channel = channelid;
+
+    fs.writeFileSync('./guilds.json', JSON.stringify(file))
+}
+
+function getCountingChannel(guildid) {
+    let file = JSON.parse(fs.readFileSync('./guilds.json'))
+    if (!file[guildid]) file[guildid] = {}
+
+    return file[guildid].channel;
+}
+
+function addToCount(guildid, userid) {
+    let file = JSON.parse(fs.readFileSync('./guilds.json'))
+    if (!file[guildid]) file[guildid] = {}
+    if (!parseInt(file[guildid].count)) file[guildid].count = 0;
+    file[guildid].count += 1;
+    file[guildid].user = userid;
+
+    fs.writeFileSync('./guilds.json', JSON.stringify(file))
+    
+    fs.writeFileSync('./counts.txt', parseInt(fs.readFileSync('./counts.txt')) + 1)
+}
+
+function getCount(guildid) {
+    let file = JSON.parse(fs.readFileSync('./guilds.json'))
+    if (!file[guildid]) file[guildid] = {}
+    if (!parseInt(file[guildid].count)) file[guildid].count = 0;
+
+    return [ file[guildid].count, file[guildid].user ];
+}
+
+function resetCount(guildid) {
+    let file = JSON.parse(fs.readFileSync('./guilds.json'))
+    if (!file[guildid]) file[guildid] = {}
+    file[guildid].count = 0;
+    file[guildid].user = "0";
+
+    fs.writeFileSync('./guilds.json', JSON.stringify(file))
+}
+
+client.login(require("./TOKEN.js").TOKEN)
